0272cac1e3fa8760fda42fdefe0da1b864519b9b
julien <julien.vince@gmail.com>
2020-02-05 14:28:56 +0100

    initial branch commit

--- src/main/java/fr/eni/jpa/entity/User.java
+++ src/main/java/fr/eni/jpa/entity/User.java
@@ -1,38 +1,58 @@
 package fr.eni.jpa.entity;
 
-import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @SuppressWarnings("serial")
 @Entity
-@Table(name = "PERSONNES_M2O")
-public class User implements Serializable {
-
+@Table(name="PERSONNES")
+public class User {
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-
+	
+	@Column(name="LOGIN", length=25)
+	private String login;
+	
+	@Column(name="PASSWORD", length=25)
+	private String password;
+	
+	@Column(name="LAST_NAME", length=50, nullable=false)
 	private String nom;
+	
+	@Column(name="FIRST_NAME", length=50, nullable=false)
 	private String prenom;
-
-	@ManyToOne
-	private Civilite civilite;
-
-	public User(String nom, String prenom, Civilite civilite) {
-		super();
-		this.nom = nom;
-		this.prenom = prenom;
-		this.civilite = civilite;
-	}
+	
+	@Column(name="BirthD", nullable=true)
+	@Temporal(TemporalType.DATE) // imposer le type en base de données
+	private Calendar dateNaissance;
+	
+	@Transient // non persistant en base de données.
+	private int age;
 
 	public User() {
 		super();
+	}
+
+	public User(String login, String password, String nom, String prenom, Calendar dateNaissance, int age) {
+		super();
+		this.login = login;
+		this.password = password;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+		this.age = age;
 	}
 
 	public int getId() {
@@ -42,6 +62,23 @@ public class User implements Serializable {
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 
 	public String getNom() {
 		return nom;
@@ -59,27 +96,45 @@ public class User implements Serializable {
 		this.prenom = prenom;
 	}
 
-	public Civilite getCivilite() {
-		return civilite;
+	public Calendar getDateNaissance() {
+		return dateNaissance;
 	}
 
-	public void setCivilite(Civilite civilite) {
-		this.civilite = civilite;
+	public void setDateNaissance(Calendar dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 	@Override
 	public String toString() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		StringBuilder builder = new StringBuilder();
 		builder.append("User [id=");
 		builder.append(id);
+		builder.append(", login=");
+		builder.append(login);
+		builder.append(", password=");
+		builder.append(password);
 		builder.append(", nom=");
 		builder.append(nom);
 		builder.append(", prenom=");
 		builder.append(prenom);
-		builder.append(", civilite=");
-		builder.append(civilite);
+		builder.append(", dateNaissance=");
+		builder.append(sdf.format(dateNaissance.getTime()));
+		builder.append(", age=");
+		builder.append(age);
 		builder.append("]");
 		return builder.toString();
 	}
 
+
+	
 }
